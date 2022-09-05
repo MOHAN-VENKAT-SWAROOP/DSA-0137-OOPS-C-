44eba99f0d7152b8e4a6babb7330780b5a36daee
MOHAN-VENKAT-SWAROOP <112849848+MOHAN-VENKAT-SWAROOP@users.noreply.github.com>
2022-09-05 14:38:26 +0530

    Add files via upload

---
+++ if elseif statement.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	x=8;
+	y=7;
+	if(x > y)
+	{
+		cout << " x is greater";
+	}
+	else if(y > x)
+	{
+		cout << " y is greater";
+	}
+    else 
+	{
+		cout << " both are equal ";
+	}	
+	return 0;
+}
